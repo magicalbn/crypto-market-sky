c69afabbdd3f582de6f0f9f7eced1d32da3a7719
Nehal Bhandari <nehalvbhandari@gmail.com>
2022-01-21 22:03:33 +0530

    added API call librairies, using axios for API requests

---
+++ axios/lib/cryptoAPICalls.js
@@ -0,0 +1,27 @@
+import axios from '../axiosBase'
+
+export const getAllCrypto = async () => {
+    try {
+        const { data } = await axios.get(`/getmarketsummaries`)
+        if (data.success)
+            return data.result
+        else return []
+    }
+    catch {
+        return []
+    }
+
+}
+
+export const getCryptoDetails = async (query) => {
+    try {
+        const { data } = await axios.get(`/getmarketsummary?market=${query}`)
+        if (data.success)
+            return data.result
+        else return []
+    }
+    catch {
+        return []
+    }
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 //Components
 import Layout from '../components/Layout'
+//crypto api calls
+import { getAllCrypto } from '../axios/lib/cryptoAPICalls'
 
-const Index = () =>{
+const Index = ({marketData}) => {
+    console.log(marketData)
     return (
         <Layout>
             <div className='container index'>crypto market</div>
+            
         </Layout>
     )
+}
+
+export async function getServerSideProps(context) {
+    const data = await getAllCrypto()
+    return {
+        props: { marketData: data },
+    }
 }
 
 export default Index
